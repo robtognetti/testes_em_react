353569b88cddfef6067c813535d1fa8b925b216b
BradockBr <robtognetti@gmail.com>
2022-09-25 15:10:42 -0300

    Exercicio 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+describe('Teste o componente App', () => {
+  beforeEach(() => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+  });
+  test('Testa se o topo da aplicação contém conjunto fixo de links de navegação', () => {
+    const linkHome = screen.getByText(/home/i);
+    const linkAbout = screen.getByText(/about/i);
+    const linkFavorite = screen.getByText(/Favorite Pokémons/i);
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkHome);
+    userEvent.click(linkAbout);
+    userEvent.click(linkFavorite);
+  });
+});
