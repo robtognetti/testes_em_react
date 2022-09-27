6558c8fcbb8713cc773d0e811eb575f0dfd964f7
BradockBr <robtognetti@gmail.com>
2022-09-27 20:20:58 -0300

    Exercicio 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste Pokedex', () => {
+  test('É possível clicar no botão de filtragem All', () => {
+    renderWithRouter(<App />);
+
+    const all = screen.getByRole('button', { name: 'All' });
+    expect(all).toBeInTheDocument();
+    userEvent.click(all);
+  });
+
+  test('Os botões de filtragem por tipo possuem o nome correto', () => {
+    renderWithRouter(<App />);
+
+    const array = screen.getAllByTestId('pokemon-type-button');
+
+    const pokemonsElements = ['Electric', 'Fire', 'Bug', 'Poison',
+      'Psychic', 'Normal', 'Dragon'];
+
+    array.forEach((element, index) => {
+      expect(element).toHaveTextContent(pokemonsElements[index]);
+    });
+  });
+});
